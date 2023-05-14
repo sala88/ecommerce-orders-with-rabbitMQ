8583e31d4c5b216ea8d85bd44391ef7a9b6f9794
Marco Salatin <marco.salatin@gmail.com>
2023-05-14 23:04:24 +0200

    add ui

--- rabbitmq-server.py
+++ rabbitmq-server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import config as cf
 import pika
 import json
@@ -10,40 +10,41 @@ app = Flask(__name__)
 def index():
     return 'OK'
 
-@app.route('/order/', methods = ['POST'])
+  
+@app.route('/order/', methods = ['GET', 'POST'])
 def order():
-    request_data = request.get_json()
 
-    order = {
-        'id': str(uuid.uuid4()),
-        'user_email': request_data['user_email'],
-        'product': request_data['product'],
-        'quantity': request_data['quantity']
-    }
-    print(order)
+    if request.method == 'POST':
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=cf.RMQ_HOST, 
-                                                                   port=cf.RMQ_PORT, 
-                                                                   credentials=pika.PlainCredentials(cf.RMQ_USER, cf.RMQ_PASSWORD)))
-    channel = connection.channel()
+        order = {
+            'id': str(uuid.uuid4()),
+            'user_email': request.form['user_mail'],
+            'product': request.form['product'],
+            'quantity': request.form['quantity']
+        }
+        print(order)
 
-    channel.basic_publish(
-        exchange=cf.EXCHANGE_NAME,
-        routing_key=cf.BINDING_KEY_NOTIFY,
-        body=json.dumps({'user_email': order['user_email']})
-    )
-    print(' [x] Sent notify message')
-    
-    channel.basic_publish(
-        exchange=cf.EXCHANGE_NAME,
-        routing_key=cf.BINDING_KEY_REPORT,
-        body=json.dumps(order)
-    )
-    print(' [x] Sent report message')
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=cf.RMQ_HOST, 
+                                                                    port=cf.RMQ_PORT, 
+                                                                    credentials=pika.PlainCredentials(cf.RMQ_USER, cf.RMQ_PASSWORD)))
+        channel = connection.channel()
 
-    connection.close()
-    return " [x] Sent: %", order
+        channel.basic_publish(
+            exchange=cf.EXCHANGE_NAME,
+            routing_key=cf.BINDING_KEY_NOTIFY,
+            body=json.dumps({'user_email': order['user_email']})
+        )
+        print(' [x] Sent notify message')
+        
+        channel.basic_publish(
+            exchange=cf.EXCHANGE_NAME,
+            routing_key=cf.BINDING_KEY_REPORT,
+            body=json.dumps(order)
+        )
+        print(' [x] Sent report message')
 
+        connection.close()
+    return render_template('order.html')
 
 if __name__ == '__main__':
     app.run(debug=cf.DEBUG, host=cf.HOST, port=cf.PORT)
